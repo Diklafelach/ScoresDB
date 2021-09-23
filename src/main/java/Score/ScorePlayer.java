3d545d92fb8da70724e7591c73e02416f03d73a8
Dikla Felach <diklafelach1@gmail.com>
2021-09-23 15:09:24 +0300

    adding the class of ScorePlayer that responsible for The cataracts of the DB

---
+++ src/main/java/Score/ScorePlayer.java
@@ -0,0 +1,19 @@
+package Score;
+
+public class ScorePlayer {
+
+    private int score;
+
+    public ScorePlayer(int score){
+
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
